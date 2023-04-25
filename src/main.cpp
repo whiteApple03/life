150b0ef2fb5f9f3d70bf6c922d8501c42bf02019
Sasha Dell <sahadell@mail.ru>
2023-04-25 21:11:21 +0700

    add base logic of game procces

--- src/main.cpp
+++ src/main.cpp
@@ -44,6 +44,10 @@ void get_map_from_user(Map& map)
                 = !map.field[coords.second][coords.first];
     }
 }
+void change_state(Map& map)
+{
+    return;
+}
 
 void print_field(Map& map)
 {
@@ -56,10 +60,25 @@ void print_field(Map& map)
     }
 }
 
+void game_procces(Map& map)
+{
+    char answer;
+    print_map(map);
+
+    do {
+        std::cout << "next step?(y/n): ";
+        std::cin >> answer;
+        change_state(map);
+        print_map(map);
+        std::cout << '\n' << answer;
+
+    } while (answer == 'y');
+}
+
 int main()
 {
     Map my_map;
     get_map_from_user(my_map);
-    print_field(my_map);
+    game_procces(my_map);
     return 0;
 }
