f4b4fd6c0ad3bd0cc67395774affbf404facf9ea
Sasha Dell <sahadell@mail.ru>
2023-05-06 22:10:37 +0700

    test the count of living cells

--- includes/logics.hpp
+++ includes/logics.hpp
@@ -7,8 +7,11 @@
 
 #ifndef LOGICS_HPP
 #define LOGICS_HPP
+template<typename T1, typename T2>
+int counting_live_cells(Map& map, T1 i, T2 k);
 
-int counting_live_cells(Map& map, int& i, int& k);
+// int counting_live_cells(Map& map, int& i, int& k);
+// int counting_live_cells(Map& map, int i, int k);
 
 void change_state(Map& map);
 
--- tests/tests.cpp
+++ tests/tests.cpp
@@ -1,10 +1,13 @@
 #include <ctest.h>
+#include <iostream>
 
 #include <Map.hpp>
 #include <input.hpp>
 #include <logics.hpp>
 
-CTEST(logic, createmap)
+Map field = {9, 9};
+
+void create_field()
 {
     bool map[9][9]
             = {{0, 1, 0, 0, 0, 0, 0, 0, 0},
@@ -13,11 +16,27 @@ CTEST(logic, createmap)
                {1, 0, 1, 0, 0, 0, 0, 0, 0},
                {0, 1, 0, 0, 0, 0, 0, 0, 0},
                {0, 0, 0, 0, 0, 0, 0, 0, 0},
-               {0, 0, 0, 0, 0, 0, 0, 0, 0},
-               {0, 0, 0, 0, 0, 0, 0, 0, 0},
-               {0, 0, 0, 0, 0, 0, 0, 0, 0}};
-    Map field = {9, 9};
-		allocate_memory_for_field(field);
-		field.field = (bool**)map;
-		
+               {0, 0, 0, 0, 0, 1, 1, 1, 0},
+               {0, 0, 0, 0, 0, 1, 0, 1, 0},
+               {0, 0, 0, 0, 0, 1, 1, 1, 0}};
+    allocate_memory_for_field(field);
+    for (int i = 0; i < field.sizeY; i++) {
+        for (int k = 0; k < field.sizeX; k++) {
+            field.field[i][k] = map[i][k];
+        }
+    }
+}
+
+CTEST(logic, createmap)
+{
+    create_field();
+}
+
+CTEST(logic, countlivecell)
+{
+    ASSERT_EQUAL(8, counting_live_cells(field, 7, 6));
+    ASSERT_EQUAL(2, counting_live_cells(field, 2, 2));
+    ASSERT_EQUAL(2, counting_live_cells(field, 5, 5));
+    ASSERT_EQUAL(0, counting_live_cells(field, 0, 8));
+    ASSERT_EQUAL(2, counting_live_cells(field, 0, 0));
 }
